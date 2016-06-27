a8c030989ca52bad880bb268760a327d1525f867
Ken Lin <kenlin@kkbox.com>
2016-06-27 11:34:01 +0800

    Revert "GREEN, it should score 14 with one spare"
    This reverts commit 391106ee8d1f88e4e7e37e108cefe6146ff9da95.

--- src/Twk/BowlingGame.php
+++ src/Twk/BowlingGame.php
@@ -18,12 +18,6 @@ class BowlingGame
     {
         $this->score = 0;
         for ($i = 0; $i < count($this->rolls); $i ++) {
-            if ( $i >= 2
-                && $this->rolls[$i - 1] + $this->rolls[$i - 2] == 10
-            ) {
-                $this->score += $this->rolls[$i];
-            }
-
             $this->score += $this->rolls[$i];
         }
 
