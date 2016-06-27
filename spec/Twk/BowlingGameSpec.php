cd927d39556fae4e8b50eb272065d9bcdb9a2fd5
Ken Lin <kenlin@kkbox.com>
2016-06-27 10:43:12 +0800

    RED, it should score 0

--- spec/Twk/BowlingGameSpec.php
+++ spec/Twk/BowlingGameSpec.php
@@ -11,4 +11,13 @@ class BowlingGameSpec extends ObjectBehavior
     {
         $this->shouldHaveType('Twk\BowlingGame');
     }
+
+    public function it_should_score_0()
+    {
+        for ($i = 1; $i <= 20; $i++) {
+            $this->roll(0);
+        }
+
+        $this->score()->shouldReturn(0);
+    }
 }
