e65791a22d2667d4ae94d00ac0220041c0713814
Ken Lin <kenlin@kkbox.com>
2016-06-27 11:40:29 +0800

    GREEN, it should score 14 with one spare

--- src/Twk/BowlingGame.php
+++ src/Twk/BowlingGame.php
@@ -19,8 +19,13 @@ class BowlingGame
         $frame = 0;
         $i = 0;
         for ($frame = 0; $frame < 10; $frame ++) {
-            $this->score += $this->rolls[$i] + $this->rolls[$i + 1];
-            $i += 2;
+            if ( $this->rolls[$i] + $this->rolls[$i + 1] == 10) {
+                $this->score += 10 + $this->rolls[$i + 2];
+                $i += 2;
+            } else {
+                $this->score += $this->rolls[$i] + $this->rolls[$i + 1];
+                $i += 2;
+            }
         }
 
         return $this->score;
