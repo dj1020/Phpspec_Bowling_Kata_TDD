51be3ba8470b2d9873919518abc99d1c91b7abb5
Ken Lin <kenlin@kkbox.com>
2016-06-27 11:04:26 +0800

    REFACTOR, it should score 20

--- src/Twk/BowlingGame.php
+++ src/Twk/BowlingGame.php
@@ -5,14 +5,22 @@ namespace Twk;
 class BowlingGame
 {
     private $score = 0;
+    private $rolls = [];
+    private $rollIndex = 0;
 
     public function roll($pins)
     {
-        $this->score += $pins;
+        $this->rolls[$this->rollIndex] = $pins;
+        $this->rollIndex ++;
     }
 
     public function score()
     {
+        $this->score = 0;
+        for ($i = 0; $i < count($this->rolls); $i ++) {
+            $this->score += $this->rolls[$i];
+        }
+
         return $this->score;
     }
 }
