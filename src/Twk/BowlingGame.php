1146c9c015a6a37f4d44e279da32fc757979ad1d
Ken Lin <kenlin@kkbox.com>
2016-06-27 10:43:47 +0800

    GREEN, it should score 0

--- src/Twk/BowlingGame.php
+++ src/Twk/BowlingGame.php
@@ -4,4 +4,13 @@ namespace Twk;
 
 class BowlingGame
 {
+    public function roll($argument1)
+    {
+        // TODO: write logic here
+    }
+
+    public function score()
+    {
+        return 0;
+    }
 }
